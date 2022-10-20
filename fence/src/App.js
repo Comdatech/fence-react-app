b446683d7b175e911f360ceadcee8d4241ccbb15
Isheanesu Valentine <zvkish001@myuct.ac.za>
2022-10-20 16:55:03 +0200

    function app to class app for authentication

--- fence/src/App.js
+++ fence/src/App.js
@@ -5,19 +5,26 @@ import Login from "./pages/Authentication/Login";
 import ForgotPassword from "./pages/Authentication/ForgotPassword";
 import SignUp from "./pages/Authentication/SignUp";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-
-          {/* Authentication Page routes*/}
-          <Route index path="/" element={<Login/>} />
-          <Route path="/forgot-password" element={<ForgotPassword/>}/>
-          <Route path="/sign-up" element={<SignUp/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isUserAuthenticated: false,
+    };
+  }
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            {/* Authentication Page routes*/}
+            <Route index path="/" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 export default App;
