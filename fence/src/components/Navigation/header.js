5e94c8eb28d420a258e2a9d2f9913c0fa33a5a47
Isheanesu Valentine <zvkish001@myuct.ac.za>
2022-11-03 14:17:53 +0200

    notificatications menu without the the Menu List

--- fence/src/components/Navigation/header.js
+++ fence/src/components/Navigation/header.js
@@ -15,10 +15,10 @@ import {
   MenuDivider,
   Avatar,
   Flex,
-  Box,
   Button,
   Center,
   Text,
+  IconButton,
 } from "@chakra-ui/react";
 import {
   faUser,
@@ -27,28 +27,45 @@ import {
   faLifeRing,
   faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
-
+import { faBell } from '@fortawesome/free-regular-svg-icons';
 
 const Header = () => {
 
-  //define fontawesome icons 
+  //fontawesome icons 
     const userIcon = <FontAwesomeIcon className="icon" icon={faUser} />;
     const settingsIcon = <FontAwesomeIcon className="icon" icon={faGear} />;
     const logoutIcon = <FontAwesomeIcon className="icon" icon={faArrowRightFromBracket} />;
     const helpIcon = <FontAwesomeIcon className="icon" icon={faLifeRing} />;
     const downIcon =  <FontAwesomeIcon className="icon" icon={faChevronDown} />;
+    const notificationIcon = <FontAwesomeIcon className='icon' icon={faBell}/>;
 
-    //dynamic variables will be decalred here 
+    //dynamic variables will be decalred here at a later stage
     const username = "Dummy Name";
+
   return (
     <>
-      <Flex>
+      <Flex direction="row" justify="space-between" className='pt-3'>
         <Flex></Flex>
         <Flex>
-          <Box></Box>
+          <Menu isLazy className="notifications-dropdown">
+            <MenuButton
+              as={IconButton}
+              icon={notificationIcon}
+              className="notifications-icon  mr-4"
+              _active={{ color: "#259237" }}
+              _hover={{ color: "#259237" }}
+            />
+            <MenuList></MenuList>
+          </Menu>
           <Menu isLazy className="profile-dropdown">
-            <MenuButton className="profile-btn" as={Button} rightIcon={downIcon}>
-              <Center className='container'>
+            <MenuButton
+              className="profile-btn mr-4"
+              as={Button}
+              rightIcon={downIcon}
+              _active={{ color: "#259237" }}
+              _hover={{ color: "#259237" }}
+            >
+              <Center className="container">
                 <Avatar
                   bg="green.500"
                   size="sm"
@@ -56,7 +73,9 @@ const Header = () => {
                   src="dummy pic here"
                   className="avatar"
                 />
-                <Text paddingLeft="1" className="username">{username}</Text>
+                <Text paddingLeft="1" className="username">
+                  {username}
+                </Text>
               </Center>
             </MenuButton>
             <MenuList>
