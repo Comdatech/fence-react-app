282d68a0e2603d5da0f9980b3dfbc16c911badba
Isheanesu Valentine <zvkish001@myuct.ac.za>
2022-11-09 18:11:45 +0200

    dummy user data json

---
+++ fence/src/data/user_data.js
@@ -0,0 +1,15 @@
+export const User=[
+    {
+        username: "South African Fake Sugar Association",
+        email: "fsa@comdatech.co.za",
+        alias: "FSA",
+        phone: "123456789",
+        address: {
+            street: "",
+            town: "",
+            city: "",
+            postal_code: "",
+            country: ""
+        }
+    }
+];
