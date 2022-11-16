dcfb9dc1c51fa5916f5af8248e6017696ca461ec
0x0J <jethrodw@protonmail.com>
2022-11-16 15:57:35 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
